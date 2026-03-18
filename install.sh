e1e3de74f56696fb37e6ee0f6292b3c1a30c6a4a
Professor-Andy <Austin.Anderson@stmartin.edu>
2026-03-18 17:23:02 +0000

    sdf

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
+export DEBIAN_FRONTEND=noninteractive
+
 sudo apt remove $(dpkg --get-selections docker.io docker-compose docker-compose-v2 docker-doc podman-docker containerd runc | cut -f1)
 # Add Docker's official GPG key:
-sudo apt update
-sudo apt install ca-certificates curl
+sudo apt update -y
+sudo apt install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -15,14 +17,14 @@ Components: stable
 Signed-By: /etc/apt/keyrings/docker.asc
 EOF
 
-sudo apt update > /dev/null
-sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin > /dev/null
+sudo apt update -y
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo systemctl start docker
 sudo usermod -aG docker $USER
 newgrp docker
 docker compose version
 docker run --rm hello-world | grep -q "Hello" && echo -e "\033[92mSuccess\033[0m" || echo -e "\033[31mFailure\033[0m"
-sudo apt install open-vm-tools open-vm-tools-desktop > /dev/null
+sudo apt install -y open-vm-tools open-vm-tools-desktop > /dev/null
 sudo mkdir /mnt/share
 ln -s /mnt/share /home/student/share
 echo ".host:/Share   /mnt/share   fuse.vmhgfs-fuse   defaults,allow_other   0   0" | sudo tee -a /etc/fstab
